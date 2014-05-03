d1b28033ae08ebb612c0f6623c3cadf6c865393e
Brandon Gavett <begavett@yahoo.com>
2014-05-02 20:54:01 -0600

    Fix

--- server.R
+++ server.R
@@ -58,8 +58,8 @@ shinyServer(function(input, output, session) {
                                               44:46 = 8;
                                               47:52 = 9;
                                               53:100 = 10"))    
-    all.data <- rbind(new.data, midus.data)
-      f.scores <- data.frame(predict(model.data, newdata = all.data))
+    all.data <- data.frame(rbind(new.data, midus.data))
+    f.scores <- data.frame(predict(model.data, newdata = all.data))
     names(f.scores) <- c("Unadjusted","NS","RG")
     f.scores <- f.scores[1,]
     f.scores <- f.scores[-2]
